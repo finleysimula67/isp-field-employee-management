3439a40f53f9c1c77e2611621909d72b7064ee6b
Suckscorpio999 <olinabin214@gmail.com>
2026-06-16 17:19:22 +0545

    Force IPv4 stack in static block to fix OAuth token exchange 'Network is unreachable' on Render

--- allinone-backend/src/main/java/com/workflow/WorkflowApplication.java
+++ allinone-backend/src/main/java/com/workflow/WorkflowApplication.java
@@ -7,6 +7,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class WorkflowApplication {
+    static {
+        System.setProperty("java.net.preferIPv4Stack", "true");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(WorkflowApplication.class, args);
     }

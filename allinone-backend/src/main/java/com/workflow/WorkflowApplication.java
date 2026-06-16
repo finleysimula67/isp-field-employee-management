89d6c0f11b9795abdc9be97977d82512ef3d8efd
Suckscorpio999 <olinabin214@gmail.com>
2026-06-16 18:12:04 +0545

    Remove ineffective static block for preferIPv4Stack -- Render's JAVA_TOOL_OPTIONS overrides it

--- allinone-backend/src/main/java/com/workflow/WorkflowApplication.java
+++ allinone-backend/src/main/java/com/workflow/WorkflowApplication.java
@@ -7,10 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class WorkflowApplication {
-    static {
-        System.setProperty("java.net.preferIPv4Stack", "true");
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(WorkflowApplication.class, args);
     }
